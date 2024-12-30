083a0b85d7c7124c48d0afe9e7bb492e81df4455
Harald Antonsen <harald.antonsen@gmail.com>
2024-12-30 22:37:22 +0100

    Day19 - part1

--- 2024/day_19/src/bin/part1.rs
+++ 2024/day_19/src/bin/part1.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+
 fn main() {
     println!("Part1!");
 
@@ -6,8 +9,50 @@ fn main() {
     dbg!(output);
 }
 
+fn parse(input: &str) -> (Vec<&str>, Vec<&str>) {
+    let (towels, targets) = input.split_once("\n\n").unwrap();
+    (
+        towels.split(",").map(|t| t.trim()).collect(),
+        targets.lines().collect(),
+    )
+}
+
+fn can_create<'a>(pattern: &'a str, towels: &Vec<&str>, hash:&mut HashMap<&'a str, bool>) -> bool {
+    if let Some(possible) = hash.get(pattern){
+        return *possible;
+    }
+    if pattern.len() == 0 {
+        // println!("success!");
+        return true;
+    }
+    for towel in towels {
+        // println!("Checking if {pattern} starts with {towel}");
+        if let Some(remaining_pattern) = pattern.strip_prefix(towel) {
+            let ret = can_create(remaining_pattern, towels, hash);
+            hash.insert(pattern, ret);
+
+            if ret {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn part1(input: &str) -> i32 {
-    0
+    let (towels, targets) = parse(input);
+
+    let mut ret = 0;
+    let mut hash = HashMap::new();
+
+    for target in targets {
+        println!("Target: {target}");
+        if can_create(target, &towels, &mut hash) {
+            ret += 1;
+        }
+        println!();
+    }
+    ret
 }
 
 #[cfg(test)]
@@ -17,8 +62,17 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let dummy = "1123";
+        let dummy = "r, wr, b, g, bwu, rb, gb, br
 
-        assert_eq!(part1(dummy), 1);
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+
+        assert_eq!(part1(dummy), 6);
     }
 }
