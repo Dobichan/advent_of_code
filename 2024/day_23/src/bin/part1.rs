91b3beb99b11d6570a1a92f4b07786c4978fd1da
Harald Antonsen <harald.antonsen@gmail.com>
2024-12-31 11:12:41 +0100

    Day20 - part1

--- 2024/day_23/src/bin/part1.rs
+++ 2024/day_23/src/bin/part1.rs
@@ -6,6 +6,47 @@ fn main() {
     dbg!(output);
 }
 
+#[derive(Debug)]
+struct Connection {
+    nodes: Vec<String>,
+}
+
+fn parse(input: &str) -> Vec<Connection> {
+    let mut ret: Vec<Connection> = vec![];
+
+    for line in input.lines() {
+        let (a, b) = line.split_once('-').unwrap();
+
+        println!("{:?} - {:?}", a, b);
+
+        let mut conn = Connection { nodes: Vec::new() };
+
+        let mut added = false;
+
+        for connection in &mut ret {
+            if connection.nodes.iter().any(|s| s == a) {
+                // println!("{:?} contains from line {:?}", connection, line);
+                connection.nodes.push(b.to_string());
+                added = true;
+                break;
+            } else if connection.nodes.iter().any(|s| a == b) {
+                connection.nodes.push(a.to_string());
+                added = true;
+                break;
+            }
+        }
+        if !added {
+            conn.nodes.push(a.to_string());
+            conn.nodes.push(b.to_string());
+            ret.push(conn);
+        }
+        println!("{:?}", ret);
+        println!();
+    }
+
+    ret
+}
+
 fn part1(input: &str) -> i32 {
     0
 }
@@ -17,7 +58,41 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let dummy = "1123";
+        let dummy = "kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn";
+
+        let temp = parse(&dummy);
+        println!("{:?}", temp);
 
         assert_eq!(part1(dummy), 1);
     }
